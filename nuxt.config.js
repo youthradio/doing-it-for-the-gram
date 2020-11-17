b3ce1eb990111ebc62faf8100d07a06182e0bfae
Radames Ajna <radamajna@gmail.com>
2020-11-17 11:43:49 -0800

    fix css preload

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,21 +80,18 @@ export default {
         href: 'favicon/safari-pinned-tab.svg'
       },
       {
-        rel: 'preload',
-
-        href: 'https://yr.media/statics/yr-media-typography/yr-typography.css',
-        as: 'style',
-        onload: "this.onload=null;this.rel='stylesheet'"
-      }
-    ],
-    noscript: [
+        rel: 'preload stylesheet',
+        href:
+          'https://interactive.yr.media/yr-media-typography/yr-typography.css',
+        as: 'style'
+      },
       {
-        innerHTML:
-          '<link rel="stylesheet" type="text/css" href="https://yr.media/statics/yr-media-typography/yr-typography.css">',
-        body: true
+        rel: 'preload stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+        as: 'style'
       }
-    ],
-    __dangerouslyDisableSanitizers: ['noscript']
+    ]
   },
   /*
    ** Customize the progress-bar color
